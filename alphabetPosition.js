4be2fe4b909d8aad3e96e7caa2904449977ebec4
nikolay <n_yozov@hotmail.com>
2022-06-27 16:44:56 -0400

    spelling

--- alphabetPosition.js
+++ alphabetPosition.js
@@ -1,7 +1,7 @@
 // In this kata you are required to, given a string,
 // replace every letter with its position in the alphabet.
 // If anything in the text isn't a letter, ignore it and don't return it.
-
+//
 
 
 function alphabetPosition(text) {
